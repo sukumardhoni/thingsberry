a8dc94a7459bec81b73f66c8851cfba7f62e3451
vinodhko <vinodhko@globaltechminds.com>
2016-04-23 18:49:29 +0530

    Removed carousel functionality

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -89,32 +89,31 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 
     /*Carousel Functionality*/
 
+/*(function () {
+  // setup your carousels as you normally would using JS
+  // or via data attributes according to the documentation
+  // http://getbootstrap.com/javascript/#carousel
+  $('#carouselivo').carousel({
+    interval: 5000
+  });
 
-    (function () {
-      // setup your carousels as you normally would using JS
-      // or via data attributes according to the documentation
-      // http://getbootstrap.com/javascript/#carousel
-      $('#carouselivo').carousel({
-        interval: 5000
-      });
+}());
 
-    }());
-
-    (function () {
-      $('.carousel-showmanymoveone .item').each(function () {
-        var itemToClone = $(this);
-        for (var i = 1; i < 4; i++) {
-          itemToClone = itemToClone.next();
-          // wrap around if at end of item collection
-          if (!itemToClone.length) {
-            itemToClone = $(this).siblings(':first');
-          }
-          // grab item, clone, add marker class, add to collection
-          itemToClone.children(':first-child').clone()
-            .addClass("cloneditem-" + (i))
-            .appendTo($(this));
-        }
-      });
-    }());
+(function () {
+  $('.carousel-showmanymoveone .item').each(function () {
+    var itemToClone = $(this);
+    for (var i = 1; i < 4; i++) {
+      itemToClone = itemToClone.next();
+      // wrap around if at end of item collection
+      if (!itemToClone.length) {
+        itemToClone = $(this).siblings(':first');
+      }
+      // grab item, clone, add marker class, add to collection
+      itemToClone.children(':first-child').clone()
+        .addClass("cloneditem-" + (i))
+        .appendTo($(this));
+    }
+  });
+}());*/
   }
 ]);
