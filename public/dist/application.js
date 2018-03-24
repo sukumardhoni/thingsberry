fa75297dce288f6f51137fbb7b00e95942f0d271
Venkat K <venkatk@Venkats-MacBook-Air.local>
2018-03-24 16:14:30 -0300

    test change after successful rebase

--- public/dist/application.js
+++ public/dist/application.js
@@ -4489,6 +4489,7 @@ angular.module('core')
       },
       templateUrl: 'modules/core/client/views/directive-partials/new-tb-clients-display.html',
       link: function (scope, elem, attrs) {
+          console.log("tbClients directive laoded");
         // console.log("entering into the clients link furnctions");
         // console.log(scope.clients);
 
